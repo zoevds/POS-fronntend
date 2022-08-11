69f0f26229938fc27a37c6c8150f2f6c9ea29801
zoevds <zoevanderschyff13@gmail.com>
2022-08-11 16:11:22 +0200

    fetching data

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
-let bodyElement = document.body,
-  content = document.querySelector(".content-wrapper"),
-  openButton = document.getElementById("open-button"),
-  closeButton = document.getElementById("close-button");
-let isOpen = false;
-
-openButton.addEventListener("click", toggle);
-
-if (closeButton) {
-  closeButton.addEventListener("click", toggle);
-}
-
-content.addEventListener("click", function () {
-  isOpen = bodyElement.classList.contains("js--menu-open");
-  if (isOpen) toggle();
-});
-
-function toggle() {
-  bodyElement.classList.toggle("js--menu-open");
-}
+// const projects = JSON.parse(localStorage.getItem("properties"))
+//   ? JSON.parse(localStorage.getItem("properties"))
+//   : [];
+fetch("https://point-of-salee.herokuapp.com/products", {
+  method: "get",
+})
+  .then((response) => response.json())
+  .then((data) => {
+    let products = [];
+    products = data;
+    products.forEach((product) => {
+      document.querySelector("#products").innerHTML += `
+      <div class="container">
+      <div class="card">
+    <div class="Item"  onclick='showItem(this.id)' id="${product.product_id}" >
+    <h1>${product.name}</h1>
+    <img src="${product.image}" alt="${product.image}">
+    <p>${product.descriptions}</p>
+    <p>${product.price}</p>
+    <p>${product.weight}</p>
+ </div>
+ </div>
+</div>
+    `;
+    });
+  });
